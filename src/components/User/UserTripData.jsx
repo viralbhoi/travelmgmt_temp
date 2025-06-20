930e88ae192d2a965497dbfd041c4822087d6722
viralbhoi <72viralbhoi@gmail.com>
2025-06-20 21:09:39 +0530

    Added some message if no data is there

--- src/components/User/UserTripData.jsx
+++ src/components/User/UserTripData.jsx
@@ -13,61 +13,64 @@ export default function UserTripData() {
             <h2 className="text-xl font-medium text-shadow-sm shadow-md p-4 bg-slate-800 rounded-2xl text-center text-slate-50">
                 User Trip Information
             </h2>
-            {userTrips.map((trip, index) => {
-                return (
-                    <div
-                        key={index}
-                        className="flex gap-2 flex-col md:flex-row justify-evenly p-4 m-1 bg-slate-300 rounded-2xl shadow-md"
-                    >
-                        <div className="flex flex-col flex-2/12 justify-center">
-                            <p>id:</p>
-                            <p>{trip.id}</p>
-                        </div>
-
-                        <div className="flex flex-col flex-3/12 justify-center">
-                            <p>
-                                {trip.pickup} &rarr; {trip.destination}
-                            </p>
-                        </div>
-
-                        <div className="flex flex-col flex-3/12 gap-2 justify-center">
-                            <p>Driver: </p>
-                            <p>
-                                {trip.status === "approved"
-                                    ? drivers.find(
-                                          (driver) =>
-                                              driver.email === trip.driverEmail
-                                      )?.email
-                                    : "Not Assigned"}
-                            </p>
-                        </div>
-
-                        <div className="flex flex-col flex-2/12 gap-2 justify-center">
-                            <p>Cost : </p>
-                            <p>₹ {trip.cost || 0}</p>
-                        </div>
-
-                        <div className="flex flex-col flex-2/12 justify-center">
-                            {trip.status === "rejected" ? (
-                                <p className="bg-red-500 text-slate-50 p-4 rounded-2xl">
-                                    {" "}
-                                    Rejected{" "}
+            {userTrips.length > 0 ?  (
+                userTrips.map((trip, index) => {
+                    return (
+                        <div
+                            key={index}
+                            className="flex gap-2 flex-col md:flex-row justify-evenly p-4 m-1 bg-slate-300 rounded-2xl shadow-md"
+                        >
+                            <div className="flex flex-col flex-2/12 justify-center">
+                                <p>id:</p>
+                                <p>{trip.id}</p>
+                            </div>
+    
+                            <div className="flex flex-col flex-3/12 justify-center">
+                                <p>
+                                    {trip.pickup} &rarr; {trip.destination}
                                 </p>
-                            ) : trip.status === "approved" ? (
-                                <p className="bg-green-500 text-slate-950 p-4 rounded-2xl">
-                                    {" "}
-                                    Approved{" "}
+                            </div>
+    
+                            <div className="flex flex-col flex-3/12 gap-2 justify-center">
+                                <p>Driver: </p>
+                                <p>
+                                    {trip.status === "approved"
+                                        ? drivers.find(
+                                              (driver) =>
+                                                  driver.email === trip.driverEmail
+                                          )?.email
+                                        : "Not Assigned"}
                                 </p>
-                            ) : (
-                                <p className="bg-yellow-300 text-slate-950 p-4 rounded-2xl">
-                                    {" "}
-                                    Pending{" "}
-                                </p>
-                            )}
+                            </div>
+    
+                            <div className="flex flex-col flex-2/12 gap-2 justify-center">
+                                <p>Cost : </p>
+                                <p>₹ {trip.cost || 0}</p>
+                            </div>
+    
+                            <div className="flex flex-col flex-2/12 justify-center">
+                                {trip.status === "rejected" ? (
+                                    <p className="bg-red-500 text-slate-50 p-4 rounded-2xl">
+                                        {" "}
+                                        Rejected{" "}
+                                    </p>
+                                ) : trip.status === "approved" ? (
+                                    <p className="bg-green-500 text-slate-950 p-4 rounded-2xl">
+                                        {" "}
+                                        Approved{" "}
+                                    </p>
+                                ) : (
+                                    <p className="bg-yellow-300 text-slate-950 p-4 rounded-2xl">
+                                        {" "}
+                                        Pending{" "}
+                                    </p>
+                                )}
+                            </div>
                         </div>
-                    </div>
-                );
-            })}
+                    );
+                })
+
+            ) : (<p className="p-4 bg-slate-100 text-black rounded-2xl">No trips yet, book a trip!</p>)}
         </div>
     );
 }
